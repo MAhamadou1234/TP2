497415fd01f278c63ab593ccd1e14cfbccb8efe3
boullagui <aichamaiga20234@gmail.com>
2024-06-09 11:54:34 +0200

    premiercommit

---
+++ codepython.py
@@ -0,0 +1,5 @@
+print("welcome mister and missis ")
+a=str(input("donner votre no s'il vous plait "))
+b=str(input("donner votre prenom s'il vous plait '"))
+c= print("vous vous appelez " ,a," ",b)
+print (c)
